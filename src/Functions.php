bbbb58fb35c124ae998a4f1a61a8c6d38a795bdc
Elvis Leite da SIlva <38222579+elvis7t@users.noreply.github.com>
2022-10-04 11:19:15 -0300

    Update Functions.php

--- src/Functions.php
+++ src/Functions.php
@@ -1,6 +1,6 @@
 <?php
-namespace ElvisLEite\DatabaseManager;
-use \ElvisLeite\DatabaseManager\Recordset;
+namespace ElvisLEite\RecordsetDatabase;
+use \ElvisLeite\RecordsetDatabase\Recordset;
 //sujeira embaixo do tapete :(
     error_reporting(E_ALL & E_NOTICE & E_WARNING);
     // require_once("../model/recordset.php");
